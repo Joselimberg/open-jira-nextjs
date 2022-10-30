603ca855c7f23edcad6f9d16eba545b66e78a40e
Jose <joselimberg291197@gmail.com>
2022-10-30 10:57:01 -0700

    temas y layout definidos

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,19 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { CssBaseline, ThemeProvider } from '@mui/material'
+import { lightTheme, darkTheme } from '../themes'
+
+
+
+
+
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={ darkTheme }>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+    
+  )
 }
